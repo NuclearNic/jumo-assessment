6ca393f1e03e5c9c718a75e65435e3eb4924e04b
nic <nuclearnicdev@gmail.com>
2017-05-11 01:03:41 +0200

    Removed SQLite for Heroku deploy.

--- app/models/loan_file.rb
+++ app/models/loan_file.rb
@@ -32,7 +32,7 @@ class LoanFile < ApplicationRecord
 
 	def generate_loan_file
 		
-		CSV.open("file.csv", "wb") do |csv|
+		CSV.open("public/tmp/Output.csv", "wb") do |csv|
 
 			csv << ['Network','Product','Month','Amount']		
 			loans = Loan.where('date BETWEEN ? AND ?', self.start_date, self.end_date)
